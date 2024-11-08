3bba70df074cd445ef89d1e133ece54c7c8f105b
Sergei <skozlenko409@gmail.com>
2024-11-09 01:45:54 +0700

    2

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Laba_4_prog/lab_4
-# Build directory: C:/Laba_4_prog/lab_4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
